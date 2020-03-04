0f984d0ee568808d34b63873af038124e65ef1ed
monsterlady <xuruikang6@hotmail.com>
2020-03-04 18:14:25 +0100

    Basic communication

---
+++ src/SimpleRead.java
@@ -0,0 +1,87 @@
+import javax.comm.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.TooManyListenersException;
+
+public class SimpleRead implements Runnable, SerialPortEventListener {
+    static CommPortIdentifier portId;
+    static Enumeration portList;
+
+    InputStream inputStream;
+    SerialPort serialPort;
+    Thread readThread;
+
+    public static void main(String[] args) {
+        portList = CommPortIdentifier.getPortIdentifiers();
+        while (portList.hasMoreElements()) {
+            portId = (CommPortIdentifier) portList.nextElement();
+            System.out.println(portId.getName());
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                if (portId.getName().equals("COM7")) {
+                    //                if (portId.getName().equals("/dev/term/a")) {
+                    SimpleRead reader = new SimpleRead();
+
+                }
+            }
+        }
+    }
+
+    public SimpleRead() {
+        try {
+            serialPort = (SerialPort) portId.open("SimpleReadApp", 2000);
+        } catch (PortInUseException e) {System.out.println(e);}
+        try {
+            inputStream = serialPort.getInputStream();
+        } catch (IOException e) {System.out.println(e);}
+        try {
+            serialPort.addEventListener(this);
+        } catch (TooManyListenersException e) {System.out.println(e);}
+        serialPort.notifyOnDataAvailable(true);
+        try {
+            serialPort.setSerialPortParams(9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+        } catch (UnsupportedCommOperationException e) {System.out.println(e);}
+        readThread = new Thread(this);
+        readThread.start();
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {System.out.println(e);}
+    }
+
+    public void serialEvent(SerialPortEvent event) {
+        switch(event.getEventType()) {
+            case SerialPortEvent.BI:
+            case SerialPortEvent.OE:
+            case SerialPortEvent.FE:
+            case SerialPortEvent.PE:
+            case SerialPortEvent.CD:
+            case SerialPortEvent.CTS:
+            case SerialPortEvent.DSR:
+            case SerialPortEvent.RI:
+            case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+                break;
+            case SerialPortEvent.DATA_AVAILABLE:
+                byte[] readBuffer = new byte[128];
+
+                try {
+                    while (inputStream.available() > 0) {
+                        int numBytes = inputStream.read(readBuffer);
+                    }
+                    System.out.write(readBuffer);
+                    System.out.println();
+                } catch (IOException e) {System.out.println(e);}
+                break;
+        }
+    }
+
+
+
+}
+
+
---
+++ src/SimpleWrite.java
@@ -0,0 +1,51 @@
+import javax.comm.CommPortIdentifier;
+import javax.comm.PortInUseException;
+import javax.comm.SerialPort;
+import javax.comm.UnsupportedCommOperationException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.Scanner;
+
+public class SimpleWrite {
+    static Enumeration portList;
+    static CommPortIdentifier portId;
+    static String messageString = "";
+    static SerialPort serialPort;
+    static OutputStream outputStream;
+
+    public static void main(String[] args) throws IOException{
+        portList = CommPortIdentifier.getPortIdentifiers();
+        int i;
+        while (portList.hasMoreElements()) {
+            portId = (CommPortIdentifier) portList.nextElement();
+            if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                if (portId.getName().equals("COM7")) {
+                    System.out.println("COM7 Found");
+                    // if (portId.getName().equals("/dev/term/a")) {
+                    try {
+                        serialPort = (SerialPort)
+                                portId.open("TEAM3",2000);
+                    } catch (PortInUseException e) {System.out.println(e);}
+                    try {
+                        outputStream = serialPort.getOutputStream();
+                    } catch (IOException e) {System.out.println(e);}
+                    try {
+                        serialPort.setSerialPortParams(9600,
+                                SerialPort.DATABITS_8,
+                                SerialPort.STOPBITS_1,
+                                SerialPort.PARITY_NONE);
+                    } catch (UnsupportedCommOperationException e) {System.out.println(e);}
+                   while(true){
+                       Scanner scanner = new Scanner(System.in);
+                       String hint = "Msg to send: ";
+                       System.out.println(hint);
+                       String input = scanner.nextLine();
+                       outputStream.write(input.getBytes());
+                   }
+                }
+            }
+        }
+    }
+}
+
